2449d4aa6e466dd743105c3ada50ecba11134945
Harika Duyu <harika.duyu@gmail.com>
2022-12-14 20:05:50 +0100

    try with regex

--- 04/solution.py
+++ 04/solution.py
@@ -1,44 +1,42 @@
 # from aoc2022 import get_input
 from typing import List
+import re
 
 
 def get_input() -> List[str]:
-
+    range_pairs = []
     with open('input.txt') as f:
-        num_pairs = [line.strip().split(',') for line in f.readlines()]
-    return num_pairs
+        for line in f.readlines():
+            start1, end1, start2, end2, _ = re.split(r"[^0-9]",line)
+            range_1 = range(int(start1), int(end1) + 1) 
+            range_2 = range(int(start2), int(end2) + 1) 
+            range_pairs.append([range_1, range_2])
+    return range_pairs
 
 
-def does_one_range_contain_the_other(two_ranges: List[str]) -> bool:
-    print(two_ranges)
-    ranges = []
-    for num_pair in two_ranges:
-        start, end = num_pair.split('-')
-        ranges.append(set(range(int(start), int(end) + 1)))
-
-    intersection = ranges[0] & ranges[1]
-    return len(intersection) == len(ranges[0]) or len(intersection) == len(ranges[1])
+def get_part1(range_pairs: List[range]) -> bool:
+    count = 0
+    for r in range_pairs:
+        intersection = set(r[0]) & set(r[1])
+        if len(intersection) == len(r[0]) or len(intersection) == len(r[1]):
+            count +=1
+    return count
 
 
-def does_one_range_intersect_with_the_other(two_ranges: List[str]) -> bool:
-    print(two_ranges)
-    ranges = []
-    for num_pair in two_ranges:
-        start, end = num_pair.split('-')
-        ranges.append(set(range(int(start), int(end) + 1)))
 
-    intersection = ranges[0] & ranges[1]
-    return len(intersection) > 0
+def get_part2(range_pairs: List[range]) -> bool:
+    # print(two_ranges)
+    count = 0
+    for r in range_pairs:
+        intersection = set(r[0]) & set(r[1])
+        if len(intersection) > 0:
+            count += 1
+    return count
 
 
 if __name__ == '__main__':
-    range_pairs_list = get_input()
-    count_contains_the_other = 0
-    count_intersects = 0
-    for two_ranges in range_pairs_list:
-        if does_one_range_contain_the_other(two_ranges=two_ranges):
-            count_contains_the_other += 1
-        if does_one_range_intersect_with_the_other(two_ranges=two_ranges):
-            count_intersects += 1
-    print(count_contains_the_other)
+    range_pairs = get_input()
+    count_one_contains_other = get_part1(range_pairs=range_pairs)
+    print(count_one_contains_other)
+    count_intersects = get_part2(range_pairs=range_pairs)
     print(count_intersects)
